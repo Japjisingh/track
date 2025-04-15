d14423c41d90da91700e2563e387e852d1219112
Japjisingh <japjisingh40@gmail.com>
2025-04-15 20:13:31 +0100

    Create generate-container.js

---
+++ pages/api/generate-container.js
@@ -0,0 +1,57 @@
+import fs from 'fs';
+import path from 'path';
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+  const {
+    googleAdsId,
+    tagManagerId,
+    fbPixel,
+    pinterestPixel,
+    tiktokPixel
+  } = req.body;
+
+  // Load a base GTM container template (you can refine it later)
+  const templatePath = path.join(process.cwd(), 'public', 'base-container.json');
+  const template = JSON.parse(fs.readFileSync(templatePath, 'utf-8'));
+
+  // Inject custom pixel values
+  const variables = [
+    {
+      name: "Google Ads Measurement ID",
+      type: "constant",
+      parameter: [{ key: "value", type: "template", value: googleAdsId }]
+    },
+    {
+      name: "Tag Manager Container ID",
+      type: "constant",
+      parameter: [{ key: "value", type: "template", value: tagManagerId }]
+    },
+    {
+      name: "Facebook Pixel",
+      type: "constant",
+      parameter: [{ key: "value", type: "template", value: fbPixel }]
+    },
+    {
+      name: "Pinterest Pixel",
+      type: "constant",
+      parameter: [{ key: "value", type: "template", value: pinterestPixel }]
+    },
+    {
+      name: "TikTok Pixel",
+      type: "constant",
+      parameter: [{ key: "value", type: "template", value: tiktokPixel }]
+    }
+  ];
+
+  template.containerVersion.variable.push(...variables);
+
+  const containerJSON = JSON.stringify(template, null, 2);
+
+  res.setHeader('Content-Disposition', 'attachment; filename=\"GTM-Container.json\"');
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(containerJSON);
+}
